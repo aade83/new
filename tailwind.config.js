74547cab69df6178b24064d0652d325e3dc5907f
aade83 <yadavaditya8324@gmail.com>
2024-09-19 16:11:50 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+export const mode = 'jit';
+export const purge = ['./index.html', './src/**/*.{js,ts,jsx,tsx}'];
+export const theme = {
+  extend: {},
+};
+export const variants = {};
+export const plugins = [];
